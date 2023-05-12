3a3a96065496606e3b7c8a44756fd2749a686136
MaroEmad21 <maromda877@gmail.com>
2023-05-12 15:52:14 +0300

    new code

--- gene.py
+++ gene.py
@@ -1,4 +1,21 @@
 # making a program that makes gene 
 # first of all we make a class that conatin the meaning of gene 
-class gene(sequence):
+
+# the sequence object that contain all nucleotide
+class Dna:
+    seq=[]
+    def get_sequence():
+        pass
+
+    def __init__(self, sequence):
+        self.sequence = sequence
+    pass
+
+
+
+class Gene(Dna):
+    def recog_promoter():
+        pass
+    def add_promoter():
+        pass
     pass 
