80a2fbc300d5fe0693f75d5b5b6ede82193e974f
MaroEmad21 <maromda877@gmail.com>
2023-05-14 00:18:09 +0300

    show the seq

--- gene.py
+++ gene.py
@@ -12,30 +12,28 @@ class Dna:
                 addSeq = input("what is your sequence: ")
                 seq =Dna(addSeq)
                 Dna.get_sequence(seq)  
-                print(seq, len(seq))
             except ValueError:
                 print("YOU DIDN'T TYPE ANYTHING!!!!")
-
     # function that returns the sequence for testing
     def get_sequence(self):
         seq = self.sequence
         #sequnce must be in upper case
         if seq:
-            seq.upper()
+            seq=seq.upper()
             # to check sequence if correct will continue else program will break
             Dna.check_sequence(seq)
-            return seq.upper()
         else:
             print("error")
         
     # a funcrion that check the sequence to see if it's correct or not (not random typing)
     def check_sequence(self):
-      for nucleotide in self:
-        if nucleotide =="A" or nucleotide =="G" or nucleotide =="C"or nucleotide =="T":
-            continue
-        else:
-            print(f"that's not correct the nucleotide {nucleotide} is wrong")
-            quit()
+        for nucleotide in self:
+            if nucleotide =="A" or nucleotide =="G" or nucleotide =="C"or nucleotide =="T":
+                continue
+            else:
+                print(f"that's not correct the nucleotide {nucleotide} is wrong")
+                quit()
+        print(f"sequence is [{self}] and the length is {len(self)} bp")
 
 
 
