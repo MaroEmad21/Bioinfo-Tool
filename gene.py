ce2379461c8af7a884650d7d6f4e6a77ae9a5924
MaroEmad21 <maromda877@gmail.com>
2023-05-13 15:43:40 +0300

    upper in sequence

--- gene.py
+++ gene.py
@@ -1,7 +1,7 @@
 # making a program that makes gene 
 # first of all we make a class that conatin the meaning of gene 
 
-# the sequence object that contain all nucleotide
+# the sequence object that contain all nucleotides
 class Dna:
     def __init__(self, sequence):
         self.sequence = sequence
@@ -10,15 +10,20 @@ class Dna:
     def add_sequence():
         try:
             addSeq = input("what is your sequence: ")
-            x =Dna(addSeq)
-            print(Dna.get_sequence(x))
-        except ValueError:
+            seq =Dna(addSeq)
+            return Dna.get_sequence(seq)
+        except None:
             print("YOU DIDN'T TYPE ANYTHING!!!!")
 
     # function that returns the sequence for testing
     def get_sequence(self):
-        return self.sequence
-    # a funcrion that check the sequence to see if it's correct or not 
+        seq = self.sequence
+        #sequnce must be in upper case
+        if seq:
+            return seq.upper()
+        else:
+            return seq
+    # a funcrion that check the sequence to see if it's correct or not (not random typing)
     def check_sequence(self):
         pass
 
