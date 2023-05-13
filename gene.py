25cd1bf063252140a3e34a9674c4014efd61a4d2
MaroEmad21 <maromda877@gmail.com>
2023-05-13 15:27:40 +0300

    added comments

--- gene.py
+++ gene.py
@@ -6,6 +6,7 @@ class Dna:
     def __init__(self, sequence):
         self.sequence = sequence
     
+    # a function to add sequence
     def add_sequence():
         try:
             addSeq = input("what is your sequence: ")
@@ -14,9 +15,10 @@ class Dna:
         except ValueError:
             print("YOU DIDN'T TYPE ANYTHING!!!!")
 
-
+    # function that returns the sequence for testing
     def get_sequence(self):
         return self.sequence
+    # a funcrion that check the sequence to see if it's correct or not 
     def check_sequence(self):
         pass
 
