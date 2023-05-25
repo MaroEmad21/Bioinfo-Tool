07ff2b0ebfe1ab0c6b0bc6477216c27effbf6dfb
MaroEmad21 <maromda877@gmail.com>
2023-05-25 22:07:07 +0300

    funcs added

--- gene.py
+++ gene.py
@@ -1,16 +1,21 @@
 # making a program that makes gene 
-# first of all we make a class that conatin the meaning of gene 
+import collections
 
+# first of all we make a class that conatin the meaning of gene 
 # the sequence object that contain all nucleotides
+
+
+Nucleotides= ["A","C","G","T"]
+
+
 class Dna:
     def __init__(self, sequence):
         self.sequence = sequence
     
     # a function to add sequence
-    def add_sequence():
+    def add_sequence(sequence):
             try:
-                addSeq = input("what is your sequence: ")
-                seq =Dna(addSeq)
+                seq =Dna(sequence)
                 Dna.get_sequence(seq)  
             except ValueError:
                 print("YOU DIDN'T TYPE ANYTHING!!!!")
@@ -28,20 +33,33 @@ class Dna:
     # a funcrion that check the sequence to see if it's correct or not (not random typing)
     def check_sequence(self):
         for nucleotide in self:
-            if nucleotide =="A" or nucleotide =="G" or nucleotide =="C"or nucleotide =="T":
-                continue
+            if nucleotide in Nucleotides:
+                pass
             else:
                 print(f"that's not correct the nucleotide {nucleotide} is wrong")
                 quit()
         print(f"sequence is [{self}] and the length is {len(self)} bp")
 
 
+    # a function to count the frequency of each nucleotide
+    def NucFreq(sequence):
+        return dict(collections.Counter(sequence))
+    
+
+
+    #transcription function
+    def transcription(seq):
+
+         return seq.replace("T","U")     
 
 
 
-class Gene(Dna):
+
+#gene is part of dna so I will make a subclass that gets a nucleotide sequence and then adds promoter and gives transcripts
+'''class Gene(Dna):
     def check_promoter():
         pass
     def add_promoter():
         pass
     pass 
+'''
--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 # here is th file where the app works
+
 # first we import all classes
 # this file is the main file whic will run all other files till now
+#it will be for testing at first then after finishing wi
 from gene import *
+import random
+
+
+
+randomStr=''.join([random.choice(Nucleotides)
+                   for nuc in range(20)])
 # AS I WILL USE THIS MAIN FUCTION TO BE THE MODERATOR OF ALL FUNCTIONS ANDD TO RUN ALL OPERTAIONS NEEDED
 def main():
-    answer = input("Which sequence you want to add? ")
-    if answer.lower() == 'dna':
-        Dna.add_sequence()
-    else:
-        print("that's not true")
-        
+    print(Dna.add_sequence(randomStr))
+    print(Dna.NucFreq(randomStr))
+    print(Dna.transcription(randomStr))    
             
 
 
